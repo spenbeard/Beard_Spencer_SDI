068a248adb367b98c503f8174358fbc6cfdb3719
spenbeard <SpencerBeard@Spencers-MacBook-Pro.local>
2015-09-05 22:29:09 -0700

    Array tutorial
    Doing Array Tutorial

---
+++ Array Test/js/script.js
@@ -0,0 +1,10 @@
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; // 0,1,2,3
+
+muppetNames[1] = "Rizzo";
+muppetNames[2] = "Fozzy";
+
+var num = 1;
+
+console.log(muppetNames[num]);
