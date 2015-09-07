4df7a38c9612bcb712a27fb95403269d1b0433de
spenbeard <SpencerBeard@Spencers-MacBook-Pro.local>
2015-09-07 14:31:46 -0700

    Changed initial prompt message
    Changed the initial prompt to ask for the user’s name before asking for
    monthly bills.

--- Beard_Spencer_Expressions_Assignment/js/script.js
+++ Beard_Spencer_Expressions_Assignment/js/script.js
@@ -7,7 +7,13 @@ Assignment: Expressions
 
  //welcome message and initial prompt
  alert("Hi. I am here to help you figure out how much \nextra money you have at the end of the month.");
+ var userName = prompt("First, what's your name?");
+ alert("Nice to meet you "+userName+". Let's see if I can help you out.")
+
+ console.log(userName);
+
+ //prompt user for the amount of money they spend on monthly bills
  var bills = prompt("How much do you spend on monthly bills (rent/mortgage, utilities, insurance, etc.)?");
 
  console.log(bills);
- alert(bills);
+ alert("You spend "+bills+" on monthly bills.");
