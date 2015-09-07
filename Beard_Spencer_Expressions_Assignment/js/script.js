56e58c89228b807b8ef249293e9c3b25545c8574
spenbeard <SpencerBeard@Spencers-MacBook-Pro.local>
2015-09-07 14:26:28 -0700

    Created initial prompt for calculator
    Created the initial prompt for calculator.

--- Beard_Spencer_Expressions_Assignment/js/script.js
+++ Beard_Spencer_Expressions_Assignment/js/script.js
@@ -5,3 +5,9 @@ Assignment: Expressions
 9/6/2015
  */
 
+ //welcome message and initial prompt
+ alert("Hi. I am here to help you figure out how much \nextra money you have at the end of the month.");
+ var bills = prompt("How much do you spend on monthly bills (rent/mortgage, utilities, insurance, etc.)?");
+
+ console.log(bills);
+ alert(bills);
