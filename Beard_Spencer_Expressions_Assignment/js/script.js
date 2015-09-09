618a9ea871f21bb713892e469edd0ec81fed7720
spenbeard <SpencerBeard@Spencers-MacBook-Pro.local>
2015-09-08 20:27:01 -0700

    Finalized rough draft of code
    Gave money left over at the end of the month. Added $ removal. Various
    bug fixes.

--- Beard_Spencer_Expressions_Assignment/js/script.js
+++ Beard_Spencer_Expressions_Assignment/js/script.js
@@ -8,24 +8,27 @@ Assignment: Expressions
  //welcome message and initial prompt
  alert("Hi. I am here to help you figure out how much \nextra money you have at the end of the month.");
  var userName = prompt("First, what's your name?");
- alert("Nice to meet you " + userName + ". Let's see if I can help you out. Oh, and do me a favor please leave out the $ in your answers. Thank you.");
+ alert("Nice to meet you " + userName + ". Let's see if I can help you out.");
 
  console.log(userName);
 
  //prompt user for the amount of money they spend on monthly bills
  var bills = prompt("How much do you spend on monthly bills (rent/mortgage, utilities, insurance, etc.)?");
+bills = bills.replace("$", "");
 
  console.log(bills);
 
  //show user what they entered for bills and prompt for food spending
  //alert("You spend $"+bills+" on monthly bills.");
  var groceries = prompt("You spend $" + bills + " on monthly bills. \nI'm sure you eat food, right? How much would you say you spend on food each month?");
+groceries = groceries.replace("$", "");
 
  console.log(groceries);
 
  //show user what they entered for groceries and prompt for other expenses
  //alert("You spend $"+groceries+" on food every month.");
  var otherExpenses = prompt("You spend $" + groceries + " on food every month. \nIf there are any other regular expenses you have, total them up. How much do you" + " spend on other regular expenses?");
+otherExpenses = otherExpenses.replace("$", "");
 
  console.log(otherExpenses);
  alert("You spend $"+otherExpenses+" on all other expenses.");
@@ -44,17 +47,29 @@ Assignment: Expressions
 
  console.log(incomeTypes);
 
- //prompt user for hourly wage, hours per week, and extra income
+ //prompt user for hourly wage
  incomeTypes[0] = prompt("How much are you paid per hour at your job?");
+ incomeTypes[0] = incomeTypes[0].replace("$", "");
  console.log(incomeTypes[0]);
+
+ //prompt user for hours per week worked
  incomeTypes[1] = prompt("How many hours do you work every week?");
+ incomeTypes[1] = incomeTypes[1].replace("$", "");
  console.log(incomeTypes[1]);
+
+ //prompt user for other income amount
  incomeTypes[2] = prompt("If you have other income sources, how much do you make from those every month?");
- alert("You make $"+incomeTypes[2]+" from other sources.");
+ incomeTypes[2] = incomeTypes[2].replace("$", "");
  console.log(incomeTypes[2]);
 
  alert("You work "+incomeTypes[1]+" hours per week. \nYou are paid $"+incomeTypes[0]+" per hour. \nYou earn $" + incomeTypes[2] + " additional income.");
 
  //set total income
  var totalIncome = (incomeTypes[0] * incomeTypes[1] * 52)/12 + incomeTypes[2];
+ totalIncome = parseFloat(totalIncome).toFixed(2);
+ alert("You earn a total of $" + totalIncome +" every month.");
  console.log(totalIncome);
+
+ //tell user how much money they have left over
+ var extraMoney = totalIncome - totalExpenses;
+ alert("You have " + extraMoney + " left over for the month. Spend it well.");
