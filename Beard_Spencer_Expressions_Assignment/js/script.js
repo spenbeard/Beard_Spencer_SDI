b1335beb5d9b5e6e3904cbd594c9eac6c40e2684
spenbeard <SpencerBeard@Spencers-MacBook-Pro.local>
2015-09-09 19:22:49 -0700

    Final fixes
    Resolved remaining bugs. Improved code by removing excessive code
    lines. Added results in comments at the end of the code.

--- Beard_Spencer_Expressions_Assignment/js/script.js
+++ Beard_Spencer_Expressions_Assignment/js/script.js
@@ -5,112 +5,137 @@ Assignment: Expressions
 9/6/2015
  */
 
+ //greeting message:
  alert("Hi. I am here to help you figure out how much \nextra money you have at the end of the month.");
- var userName = prompt("First, what's your name?");
- alert("Nice to meet you " + userName + ". Let's see if I can help you out.");
 
- console.log(userName);
+ //prompt for user's name
+ var userName = prompt("First, what's your name?");
+
+ console.log("Nice to meet you, " + userName + ".");
 
  //prompt user for the amount of money they spend on monthly bills
  var bills = prompt("How much do you spend on monthly bills (rent/mortgage, utilities, insurance, etc.)?");
+
+ //remove $ or commas from the string entered for bills, which would cause the string to parse incorrectly
  bills = bills.replace("$", "");
  bills = bills.replace(",", "");
 
- console.log(bills);
+ console.log("You spend $" + parseFloat(bills).toFixed(2) + " on monthly bills.");
 
- //show user what they entered for bills and prompt for food spending
- //alert("You spend $"+bills+" on monthly bills.");
- var groceries = prompt("You spend $" + parseFloat(bills).toFixed(2) + " on monthly bills. \nI'm sure you eat food, right? How much would you say you spend on food each month?");
+ //restate to the user what they entered for bills and prompt user for the amount of money they spend on food
+ var groceries = prompt("I'm sure you eat food, right? How much would you say you spend on food each month?");
+
+ //remove $ or commas from the string entered for groceries, which would cause the string to parse incorrectly
  groceries = groceries.replace("$", "");
  groceries = groceries.replace(",", "");
 
- console.log(groceries);
+ console.log("You spend $" + parseFloat(groceries).toFixed(2) + " on food every month.");
 
+//restate to the user what they entered for groceries and prompt the user for the total amount of money spent on any other regular expenses
+ var otherExpenses = prompt("If there are any other regular expenses you have, total them up. How much do you" + " spend on other regular expenses?");
 
- var otherExpenses = prompt("You spend $" + parseFloat(groceries).toFixed(2) + " on food every month. \nIf there are any other regular expenses you have, total them up. How much do you" + " spend on other regular expenses?");
+ //remove $ and commas from the string entered for otherExpenses, which would cause the string to parse incorrectly
  otherExpenses = otherExpenses.replace("$", "");
  otherExpenses = otherExpenses.replace(",", "");
 
- console.log(otherExpenses);
-
- alert("You spend $" + parseFloat(otherExpenses).toFixed(2) + " on all other expenses.");
+ console.log("You spend $" + parseFloat(otherExpenses).toFixed(2) + " on all other expenses.");
 
  //total the amount of bills, groceries, and otherExpenses
  var totalExpenses = parseFloat(bills) + parseFloat(groceries) + parseFloat(otherExpenses);
 
- console.log(totalExpenses);
-
- alert(userName+", you spend $" + parseFloat(totalExpenses).toFixed(2) + " every month.");
+ console.log("You spend $" + parseFloat(totalExpenses).toFixed(2) + " every month.");
 
  //tell the user that we are now covering income amounts
  alert("Okay, "+userName+". Now it's time for us to talk about income.");
 
  //set up array for income values
- var incomeTypes = ["Hourly Pay", "Hours per week", "Other Income"];
+ var incomeTypes = ["Your hourly pay", " how many hours per week per week you work", " and any other income you have."];
 
- console.log(incomeTypes);
+ console.log("To find out how much you make, we need to know 3 things: " + incomeTypes);
 
  //prompt user for hourly wage
  incomeTypes[0] = prompt("How much are you paid per hour at your job?");
+
+ //remove $ and commas from the string entered for incomeTypes[0], which would cause the string to parse incorrectly
  incomeTypes[0] = incomeTypes[0].replace("$", "");
  incomeTypes[0] = incomeTypes[0].replace(",", "");
- console.log(incomeTypes[0]);
+
+ console.log("You are paid $" + parseFloat(incomeTypes[0]).toFixed(2) + " per hour.");
 
  //prompt user for hours per week worked
  incomeTypes[1] = prompt("How many hours do you work every week?");
+
+ //remove $ and commas from the string entered for incomeTypes[1], which would cause the string to parse incorrectly
  incomeTypes[1] = incomeTypes[1].replace("$", "");
  incomeTypes[1] = incomeTypes[1].replace(",", "");
- console.log(incomeTypes[1]);
+
+ console.log("You work " + incomeTypes[1] + " hours per week.");
 
  //prompt user for other income amount
  incomeTypes[2] = prompt("If you have other income sources, how much do you make from those every month?");
+
+ //remove $ and commas from the string entered for incomeTypes[2], which would cause the string to parse incorrectly
  incomeTypes[2] = incomeTypes[2].replace("$", "");
  incomeTypes[2] = incomeTypes[2].replace(",", "");
- console.log(incomeTypes[2]);
 
- alert("You work "+incomeTypes[1]+" hours per week. \nYou are paid $"+incomeTypes[0]+" per hour. \nYou earn $" + parseFloat(incomeTypes[2]).toFixed(2) + " additional income.");
+ console.log("You earn $" + parseFloat(incomeTypes[2]).toFixed(2) + " from all other sources.");
 
- //set total income
+ //determine yearly salary
  var workIncome = incomeTypes[0] * incomeTypes[1] * 52;
- workIncome /= 12;
- console.log(parseFloat(workIncome).toFixed(2));
- var totalIncome = parseFloat(workIncome) + parseFloat(incomeTypes[2]);
- console.log(totalIncome);
- alert("You earn a total of $" + parseFloat(totalIncome).toFixed(2) +" every month.");
 
- //tell user how much money they have left over
+ //determine average monthly salary
+ workIncome /= 12;
+
+ console.log("You make $" + parseFloat(workIncome).toFixed(2) + " per month at your job.");
+
+ //determine total income by adding workIncome to incomeTypes
+ var totalIncome = parseFloat(workIncome) + parseFloat(incomeTypes[2]);
+ console.log("You earn $" + parseFloat(totalIncome).toFixed(2) + " every month.");
+
+ //determine how much money the user has left over after subtracting expenses from income
  var extraMoney = totalIncome - totalExpenses;
- console.log(extraMoney);
- alert(userName + ", you earn a total of $" + parseFloat(totalIncome).toFixed(2) +" every month. \nYou spend $" + parseFloat(totalExpenses).toFixed(2) + " every month. \nYou have $" + parseFloat(extraMoney).toFixed(2) + " left over for the month. \nI hope this helps you.");
+
+ console.log("After you pay for all of your expenses, you will have $" + parseFloat(extraMoney).toFixed(2) + " left over. I hope this helps.");
 
  /* test values
 
-Inputs
-  userName = Spencer
-  bills = $1,200.00
-  groceries = $300
-  otherExpenses = 200
-  incomeTypes[0] = $13.00
-  incomeTypes[1] = 40
-  incomeTypes[2] = $0
-Outputs
-  totalExpenses = 1700.00
-  workIncome = 2253.3333333333335
-  totalIncome = 2253.3333333333335
-  extraMoney = 553.3333333333335
+  Nice to meet you, Spencer.
+  You spend $1200.00 on monthly bills.
+  You spend $300.00 on food every month.
+  You spend $200.00 on all other expenses.
+  You spend $1700.00 every month.
+  To find out how much you make, we need to know 3 things: Your hourly pay,how many hours per week per week you work,and any other income you have.
+  You are paid $13.00 per hour.
+  You work 40 hours per week.
+  You earn $0.00 from all other sources.
+  You make $2253.33 per month at your job.
+  You earn $2253.33 every month.
+  After you pay for all of your expenses, you will have $553.33 left over. I hope this helps.
 
-Inputs
-  userName = Alexander
-  bills = 1500
-  groceries = 500
-  otherExpenses = $750
-  incomeTypes[0] = 10.25
-  incomeTypes[1] = 25
-  incomeTypes[2] = $500
-Outputs
-  totalExpenses = 2750
-  workIncome = 1110.42
-  totalIncome = 1610.4166666666667
-  extraMoney = -1139.5833333333333
+  Nice to meet you, Alexander.
+  You spend $1500.00 on monthly bills.
+  You spend $500.00 on food every month.
+  You spend $500.00 on all other expenses.
+  You spend $2500.00 every month.
+  To find out how much you make, we need to know 3 things: Your hourly pay,how many hours per week per week you work,and any other income you have.
+  You are paid $10.00 per hour.
+  You work 50 hours per week.
+  You earn $0.00 from all other sources.
+  You make $2166.67 per month at your job.
+  You earn $2166.67 every month.
+  After you pay for all of your expenses, you will have $-333.33 left over. I hope this helps.
+
+  Nice to meet you, Mr. Beard.
+  You spend $1750.00 on monthly bills.
+  You spend $600.00 on food every month.
+  You spend $1000.00 on all other expenses.
+  You spend $3350.00 every month.
+  To find out how much you make, we need to know 3 things: Your hourly pay,how many hours per week per week you work,and any other income you have.
+  You are paid $75.00 per hour.
+  You work 40 hours per week.
+  You earn $1500.00 from all other sources.
+  You make $13000.00 per month at your job.
+  You earn $14500.00 every month.
+  After you pay for all of your expenses, you will have $11150.00 left over. I hope this helps.
 
   */
