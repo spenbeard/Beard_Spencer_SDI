903281571c45797dfcfb6ccf018e999a47c4f75f
spenbeard <SpencerBeard@Spencers-MacBook-Pro.local>
2015-09-11 19:47:03 -0700

    Created Fruit Array
    Created Fruit Array

--- GoToTrainingWeek3/js/script.js
+++ GoToTrainingWeek3/js/script.js
@@ -1,8 +1,32 @@
-
  /*
 Spencer Beard
 SDI Section #1
-Title
-Date
+GoTo Week 3
+September 11, 2015
  */
 
+//Create an array of fruit names and then sort it
+
+//Array - variable that holds multiple variables
+//Index # of an array always starts with 0!!
+
+var bowlOfFruit = ["apple", "banana", "pear", "peach"];
+
+ //see whole array
+ console.log(bowlOfFruit);
+
+ //console.log the pear
+ console.log(bowlOfFruit[2]);
+
+ //How many items are in an array?
+ //length property - acces this by using dot syntax
+ //fancy term for use a period
+
+ console.log(bowlOfFruit.length);
+
+ //Count the number of pears in our fruit bowl
+
+ //Create a variable that will keep track of how many pears there are.
+ var totalNumPears = 0;
+
+ 
