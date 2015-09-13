f621c0b832d239990efa53fb79bb868e6b5de963
spenbeard <spen.beard@gmail.com>
2015-09-13 13:02:27 -0700

    Added Classes
    Created Classes. added function to make all string inputs lowercase for
    validation purposes.

--- Beard_Spencer_Conditionals_Assignment/js/script.js
+++ Beard_Spencer_Conditionals_Assignment/js/script.js
@@ -20,14 +20,19 @@ Date
      }
  }
 
+ //set up array of races available
  var availableRaces = ["Human", "Elf", "Dwarf", "Orc", "Goblin", "Undead"];
- var characterRace = prompt("Choose a race from the following: " + availableRaces);
+
+ //create a variable to track the race selected by the User
+ var characterRace = prompt("Choose a race from the following:\n" + availableRaces.join(", "));
+
+ //create a variable that is used to validate that the chosen race is one of the options
  var raceValidate = 0;
 
  for(var iR = 0; iR < 1; ) {
      for (var i = 0; i < availableRaces.length; i++) {
-         if (availableRaces[i] === characterRace) {
-             console.log("You chose to be " + characterRace);
+         if (availableRaces[i].toLowerCase() === characterRace.toLowerCase()) {
+             console.log("You chose to be a(n) " + characterRace.toLowerCase());
              i = availableRaces.length + 1;
              raceValidate++
          }
@@ -35,7 +40,28 @@ Date
 
      if (raceValidate == 0) {
          console.log("That is not one of the options. Please enter again.");
-         var characterRace = prompt("Choose a race from the following: " + availableRaces);
+         characterRace = prompt("Choose a race from the following:\n" + availableRaces.join(", "));
+     } else {
+         iR = 1;
+     }
+ }
+
+ var availableClasses = ["Warrior", "Mage", "Archer", "Cleric"];
+ var characterClass = prompt("Choose a race from the following:\n" + availableClasses.join(", "));
+ var classValidate = 0;
+
+ for(var iR = 0; iR < 1; ) {
+     for (var i = 0; i < availableClasses.length; i++) {
+         if (availableClasses[i].toLowerCase() === characterClass.toLowerCase()) {
+             console.log("You chose to be a " + characterClass.toLowerCase());
+             i = availableClasses.length + 1;
+             classValidate++
+         }
+     }
+
+     if (classValidate == 0) {
+         console.log("That is not one of the options. Please enter again.");
+         characterClass = prompt("Choose a race from the following:\n" + availableClasses.join(", "));
      } else {
          iR = 1;
      }
