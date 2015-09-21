763bfbba2a899d6105ef18a07ced8f0050e52fa0
spenbeard <spen.beard@gmail.com>
2015-09-21 13:21:34 -0700

    Prompted User for Name
    Prompted User for Name.

--- Beard_Spencer_Functions_Assignment/js/script.js
+++ Beard_Spencer_Functions_Assignment/js/script.js
@@ -7,4 +7,15 @@ Date
 
 //console.log("This is a test. This is only a test.");
 
- 
+ //Initial combat choice
+ var userName = prompt("Hello and welcome to the Arena. What is your name?");
+ while(userName === ""){
+     userName = prompt("We will not admit anyone to fight in the Arena without a name. What is your name?");
+ }
+
+ console.log("Hello, " + userName + ". Welcome to the Arena.");
+
+ /*
+ var weaponchoices = ["Sword", "Axe", "Bow"]
+ var userClass = prompt("Choose from the following options:\nSword - High base damage.\nAxe - High stun chance.\nBow - High Critical strike chance.");
+     */
