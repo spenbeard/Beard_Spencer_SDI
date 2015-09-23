bbd7011c5c8fffbe398238c8f9a4551a821814d2
spenbeard <spen.beard@gmail.com>
2015-09-22 19:50:11 -0700

    Added Enemy Selection Option
    Added an option to select what enemy you fight. Also simplified the
    function for attack.

--- Beard_Spencer_Functions_Assignment/js/script.js
+++ Beard_Spencer_Functions_Assignment/js/script.js
@@ -25,36 +25,69 @@ Date
  var weaponCritHit = [2, 3, 5];
 
  //prompt user for weapon choice
- var userWeapon = prompt("Choose from the following options:\nSword - High base damage.\nHammer - High stun chance.\nBow - High Critical strike chance.");
+ var userWeapon = prompt("What weapons do you desire to fight with?\n\nChoose from the following options:\n" + weaponChoices[0] + " - High base damage.\n" + weaponChoices[1] + " - High stun chance.\n" + weaponChoices[2] + " - High Critical strike chance.");
+ var weaponValid = 0;
 
  //validate weapon choice
- for (var i = 0; i < weaponChoices.length; ){
-     if(weaponChoices[i].toLowerCase() === userWeapon.toLowerCase()){
-         console.log("You have chosen to wield a " + userWeapon + ".");
-         weaponDamage = weaponDamage[i];
-         console.log("Your base damage is " + weaponDamage + ".");
-         weaponCritChance = weaponCritChance[i];
-         console.log("Your base critical strike chance is " + weaponCritChance*100 + "%.");
-         weaponCritHit = weaponCritHit[i];
-         console.log("Your base critical strike multiplier is " + weaponCritHit + ".");
-         i = weaponChoices.length + 1;
+ for(var iW = 0; iW < 1; ){
+     for (var i = 0; i < weaponChoices.length; i++){
+         if(weaponChoices[i].toLowerCase() === userWeapon.toLowerCase()){
+             console.log("You have chosen to wield a " + userWeapon + ".");
+             weaponDamage = weaponDamage[i];
+             console.log("Your base damage is " + weaponDamage + ".");
+             weaponCritChance = weaponCritChance[i];
+             console.log("Your base critical strike chance is " + weaponCritChance*100 + "%.");
+             weaponCritHit = weaponCritHit[i];
+             console.log("Your base critical strike multiplier is " + weaponCritHit + ".");
+             i = weaponChoices.length + 1;
+             weaponValid++;
+         }
+     }
+     if(weaponValid == 0){
+         console.log("I do not possess that weapon. Please listen carefully.");
+         userWeapon = prompt("What weapons do you desire to fight with?\n\nChoose from the following options:\n" + weaponChoices[0] + " - High base damage.\n" + weaponChoices[1] + " - High stun chance.\n" + weaponChoices[2] + " - High Critical strike chance.");
      } else{
-         userWeapon = prompt("You must select a valid weapon.\nChoose from the following options:\nHammer - High base damage.\nAxe - High stun chance.\nBow - High Critical strike chance.");
+         iW = 1;
      }
  }
 
- var damageDealt = function(offDamage, defDefense){
-     var totalDamage = Number(offDamage) - Number(defDefense);
-     return totalDamage;
- };
+ //choices for enemies. Humans will fight with swords. Dwarves will fight with hammers. Elves will fight with bows.
+ var enemyRace = ["Human", "Dwarf", "Elf"];
+ var enemyChoice = prompt("Finally, what enemy would you like to fight?\n\nChoose from the following options:\n" + enemyRace[0] + " - Prefers swords.\n" + enemyRace[1] + " - Prefers hammers.\n" + enemyRace[2] + " - Prefers bows.");
+ var enemyValid = 0;
 
- function baseAttackDamage(baseDamage, critChance, critHit){
-     var critStrike = Math.round(Math.random()*100);
-     if(critStrike <= critChance*100){
-         var atkCritDamage = Number(baseDamage)*Number(critHit);
-         return atkCritDamage;
+ //validate enemy choice
+ for(var iE = 0; iE < 1; ){
+     for (i = 0; i < enemyRace.length; i++) {
+         if(enemyRace[i].toLowerCase() === enemyChoice.toLowerCase()){
+             console.log("You have chosen to fight a(n) " + enemyChoice + ".");
+             var enemyDamage = weaponDamage[i];
+             console.log("You enemy's damage is " + enemyDamage);
+             var enemyCritChance = weaponCritChance[i];
+             console.log("Your enemy's critical strike chance is " + enemyCritChance);
+             var enemyCritHit = weaponCritHit[i];
+             console.log("Your enemy's critical strike multiplier is " + enemyCritHit);
+             enemyValid++;
+         }
+     }
+     if(enemyValid == 0){
+         console.log("You cannot fight an imaginary enemy. Please listen carefully.");
+         enemyChoice = prompt("Finally, what enemy would you like to fight?\n\nChoose from the following options:\n" + enemyRace[0] + " - Prefers swords.\n" + enemyRace[1] + " - Prefers hammers.\n" + enemyRace[2] + " - Prefers bows.");
      } else{
-         var atkDamage = baseDamage;
+         iE = 1;
+     }
+ }
+
+ //Need to calculate damage dealt. If a critical strike occurs, damage dealt = Base Damage * Critical Strike Hit multiplier. If no critical strike, damage dealt = base damage.
+ var attack = function baseAttackDamage(baseDamage, critChance, critHit){
+     var critStrike = Math.round(Math.random()*100);
+     var atkDamage;
+     if(critStrike <= critChance*100){
+         atkDamage = Number(baseDamage)*Number(critHit);
+         return atkDamage;
+     } else {
+         atkDamage = baseDamage;
          return atkDamage;
      }
- }
+ };
+
